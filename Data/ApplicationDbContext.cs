27a47660b7016c3e9cbe118d1ab10cc18a034384
MikiLand <74683061+MikiLand@users.noreply.github.com>
2023-09-06 22:55:56 +0200

    Updated database

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCSimpleCRM.Models;
+
+namespace MVCSimpleCRM.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Users> users { get; set; }
+        public DbSet<Tasks> tasks { get; set; }
+        public DbSet<TaskUsers> taskUsers { get; set; }
+        public DbSet<TaskMessages> taskMessages { get; set; }
+
+    }
+}
