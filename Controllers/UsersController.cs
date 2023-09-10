910d1388efd036059720b2b3d3e1307286df7ea2
MikiLand <74683061+MikiLand@users.noreply.github.com>
2023-09-10 17:07:54 +0200

    Loading data into UsersView

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCSimpleCRM.Data;
+using MVCSimpleCRM.Models;
 
 namespace MVCSimpleCRM.Controllers
 {
     public class UsersController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Users> users = _context.users.ToList();
+            return View(users);
         }
     }
 }
