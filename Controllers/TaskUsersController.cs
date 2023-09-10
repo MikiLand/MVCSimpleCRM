1768833f3e358c5391aaf7e6d8ed5a71ca609e08
MikiLand <74683061+MikiLand@users.noreply.github.com>
2023-09-10 16:10:28 +0200

    Added controllers and views

---
+++ Controllers/TaskUsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCSimpleCRM.Controllers
+{
+    public class TaskUsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
