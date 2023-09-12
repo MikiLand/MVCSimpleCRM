8f18211468b9bba9ec9893fcf7442ba6231f70a6
MikiLand <74683061+MikiLand@users.noreply.github.com>
2023-09-12 21:30:10 +0200

    Added UserRepository

---
+++ Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using MVCSimpleCRM.Models;
+
+namespace MVCSimpleCRM.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<Users>> GetAll();
+
+        Task<Users> GetByIdAsync(int id);
+        Task<IEnumerable<Users>> GetUserByLogin(string login);
+
+        bool Add(Users user);
+        bool Update(Users user);
+        bool Delete(Users user);
+        //bool Edit(Users user);
+        bool Save();
+    }
+}
---
+++ Repository/UserRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCSimpleCRM.Data;
+using MVCSimpleCRM.Interfaces;
+using MVCSimpleCRM.Models;
+
+namespace MVCSimpleCRM.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public bool Add(Users user)
+        {
+            _context.Add(user);
+            return Save();
+        }
+
+        public bool Delete(Users user)
+        {
+            _context.Remove(user);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Users>> GetAll()
+        {
+            return await _context.users.ToListAsync();
+        }
+
+        public async Task<Users> GetByIdAsync(int id)
+        {
+            return await _context.users.FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task<IEnumerable<Users>> GetUserByLogin(string login)
+        {
+            //return await _context.users.Where(u => u.Login == login).ToListAsync();
+            //return await _context.users.Where(u => u.Login.Contains(login) && u.Login.StartsWith("aaa")).ToListAsync();
+            return await _context.users.Where(u => u.Login.Contains(login)).ToListAsync();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(Users user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
