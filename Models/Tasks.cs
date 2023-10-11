df198b9128fc0214783c7a2651b60e42d8d057b7
MikiLand <74683061+MikiLand@users.noreply.github.com>
2023-10-11 19:24:24 +0200

    Fixed creating task

--- Models/Tasks.cs
+++ Models/Tasks.cs
@@ -6,7 +6,7 @@ namespace MVCSimpleCRM.Models
     public class Tasks
     {
         [Key]
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Status { get; set; }
