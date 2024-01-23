201eda4633376693fa9c0f65a89c72060227a37d
Mikołaj Malicki <mikiland.pl@gmail.com>
2024-01-23 14:08:22 +0100

    TaskDetailPage shows correct amount of attached users

---
+++ ViewModels/TaskUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCSimpleCRM.ViewModels
+{
+    public class TaskUserViewModel
+    {
+        public int Id { get; set; }
+        public int IdTask { get; set; }
+        public string IdUser { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
--- obj/Debug/net7.0/MVCSimpleCRM.AssemblyInfo.cs
+++ obj/Debug/net7.0/MVCSimpleCRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCSimpleCRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99c2ad9771da03a497c282c42c72adcca5fceda6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCSimpleCRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCSimpleCRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
