2784d4ef627b96c7b61001f10678edd973bb9cad
Mikołaj Malicki <mikiland.pl@gmail.com>
2024-01-31 16:17:04 +0100

    User is being passed to backend while trying to remove him

--- obj/Debug/net7.0/MVCSimpleCRM.AssemblyInfo.cs
+++ obj/Debug/net7.0/MVCSimpleCRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCSimpleCRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da15883142e486dbead5e01602d6179f11a59df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af6aee8040888eef05d4458dbf14bf6dd698dfec")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCSimpleCRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCSimpleCRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
