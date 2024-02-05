0ab089ce28c9662fb2d9b12aa3256c3b915d5c76
Mikołaj Malicki <mikiland.pl@gmail.com>
2024-02-05 12:24:00 +0100

    Fixed error with partial view not being found

--- obj/Debug/net7.0/MVCSimpleCRM.AssemblyInfo.cs
+++ obj/Debug/net7.0/MVCSimpleCRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCSimpleCRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+205c32649ad6f13e98d351c9b663d7d2fac72eaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCSimpleCRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCSimpleCRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
