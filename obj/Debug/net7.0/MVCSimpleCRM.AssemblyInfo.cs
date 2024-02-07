17877f747ce4b76193abf79f9219a511499b7f3c
Mikołaj Malicki <mikiland.pl@gmail.com>
2024-02-07 20:25:46 +0100

    Working SemiPaging of user created tasks

--- obj/Debug/net7.0/MVCSimpleCRM.AssemblyInfo.cs
+++ obj/Debug/net7.0/MVCSimpleCRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCSimpleCRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+579f7569543471ceea7d29d004d3408cf1c87847")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c10cc28fe21d43ebb4bb827d3b117788f07e2840")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCSimpleCRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCSimpleCRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
